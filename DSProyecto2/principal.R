e081c7f3040a4d37c9a47dda8a0b02aad2ea374d
davisalvarez <davisen321@gmail.com>
2018-08-19 21:30:54 -0600

    El comienzo del fin

---
+++ DSProyecto2/principal.R
@@ -0,0 +1,13 @@
+
+library(readr)
+
+
+
+#Leer arvhivo .csv
+CountriesOfTheWorldCSV <- read_csv("./data/descargas/CountriesOfTheWorld.csv")
+
+WorldHappiness2015CSV <- read_csv("./data/descargas/WorldHappiness2015.csv")
+WorldHappiness2016CSV <- read_csv("./data/descargas/WorldHappiness2016.csv")
+WorldHappiness2017CSV <- read_csv("./data/descargas/WorldHappiness2017.csv")
+#mostrar arvhivo .csv
+View(WorldHappiness2017CSV)
